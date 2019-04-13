2914d6fe29d60172c7cf06bae3938f555f6cfaea
dishdishes <rik613@i.ua>
2019-04-13 19:44:24 +0300

    first commit
    add project from mac

---
+++ Page_scrapper/Page_scrapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Safari;
+
+namespace Page_scrapper
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("web page address");
+            string link = Console.ReadLine();
+            link = "https://rozetka.com.ua";
+            if(!string.IsNullOrWhiteSpace(link))
+            {               
+               // using (WebClient client = new WebClient())
+                //{
+                  //  string htmlCode = client.DownloadString(link);
+                //} 
+                IWebDriver driver = new ChromeDriver(@"/Users/dishdishes/Downloads/chromedriver");
+                driver.Navigate().GoToUrl(@link);
+                string html = driver.PageSource;
+            }
+            Console.ReadKey();
+        }
+    }
+}
